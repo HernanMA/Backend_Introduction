6fd8cd2b91abe67eb43aa330d6d639f7aaffcf7b
Hernan <hernanariza69@gmail.com>
2024-05-27 07:50:19 -0500

    feat: :sparkles: Polimorfismo corregido

---
+++ src/Polimorfismo/Polimorfism.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Polimorfismo;
+
+/**
+ *
+ * @author hernan
+ */
+public class Polimorfism {
+    public static void main(String[] args) {
+        
+        Perro perro1 = new Perro();
+        perro1.hacerSonido();
+        
+        Gato cat = new Gato();
+        cat.hacerSonido();
+    }
+    
+}
